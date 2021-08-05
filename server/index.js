8d4dc646e5188bd5ddd59cfa0134e758e24ec653
Fahim Rahman <fahimrahman.siam>
2021-08-06 00:55:44 +0600

    first upload

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import studentRoutes from './routes/student.js';
+
+
+const app=express();
+
+
+app.use(bodyParser.json({limit:"20mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"20mb",extended:true}));
+app.use(cors());
+app.use('/students',studentRoutes);
+
+//connect with the mongodb databse
+const CONNECTION_URL="mongodb+srv://fahimSiam:fahimSiam123@fahimcluster1693.6ox9b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{
+ useNewUrlParser:true,useUnifiedTopology:true
+}).then(()=>app.listen(PORT,()=> console.log(`Connection fine and running on port: ${PORT}`))).catch((err)=>console.log(err.message));
+mongoose.set('useFindAndModify',false);
+//the last line is for avoiding warnings
